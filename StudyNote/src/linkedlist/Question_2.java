9b9de2f4b0cdfdb9a5bdb5bed5ed994bd44514eb
niexl <2249069314@qq.com>
2023-11-06 17:20:40 +0800

    链表：问题 2，删除节点

--- StudyNote/src/linkedlist/Question_2.java
+++ StudyNote/src/linkedlist/Question_2.java
@@ -7,7 +7,11 @@ package linkedlist;
  * @Project: LeetCodeStudy
  * @Package: linkedlist
  * @Version: 1.0
- * @ToDo:
+ * @ToDo: 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。返回删除后的链表的头节点。
+ * 
+ * 示例：输入: head = [4,5,1,9], val = 1
+ * 		输出: [4,5,9]
+ * 		解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
  */
 public class Question_2 {
 	public ListNode deleteNode(ListNode head, int val) {
